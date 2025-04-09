4489a0eca973dad16374e56dcd77a006478ef06a
Monika Leonardo <vinos_7518@mail.ru>
2025-04-09 04:36:40 +0300

    изменил архетектуру токенизатора

--- get_tokens.c
+++ get_tokens.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "get_tokens.h"
+//#include "get_tokens.h"
 #include "structs.h"
 
 
@@ -21,11 +21,12 @@ typedef struct token_t {
 
 */
 
-#define LIST_FUNC_STRING "sin cos tng ctng"
+//#define LIST_FUNC_STRING "sin cos tng ctng"
 
 static void __input(void);
 static char __data_correctness(void);
-static unsigned __is_token(unsigned ind, unsigned ind_token, const unsigned char *list_funcs_string);
+static unsigned __is_token(unsigned ind, unsigned ind_token);
+static char __preproces(void);
 
 static  unsigned char *string;
 static Token *arr_tokens;
@@ -34,12 +35,11 @@ static Token *arr_tokens;
 Token *
 get_tokens(void)
 {
-	const unsigned char list_funcs_string[] = LIST_FUNC_STRING;
 	__input();
 #define MIN_LEN_ARR_TOKENS 10
 	unsigned len_arr_tokens = MIN_LEN_ARR_TOKENS;
-	arr_tokens = (Token *)calloc(MIN_LEN_ARR_TOKENS, sizeof(Token));
-	if (!arr_tokens || !string) {
+	arr_tokens = (Token *)calloc(len_arr_tokens, sizeof(Token));
+	if (!arr_tokens || !string || __preproces()) {
 		free(string);
 		goto ERROR_GET_TOKENS;
 	}
@@ -54,11 +54,13 @@ get_tokens(void)
 				goto ERROR_GET_TOKENS;
 			}
 		}
-		ind += __is_token(ind, ind_token, list_funcs_string);
+		ind = __is_token(ind, ind_token);
+		/*
 		if (arr_tokens[ind_token].type == UNDEFINED_TYPE) {
 			free(string);
 			goto ERROR_GET_TOKENS;
 		}
+		*/
 	}
 
 	free(string);
@@ -96,18 +98,36 @@ __input(void)
 }
 
 
-#define IS_NUMBER(s) (s >= '0' && s <= '9')
-#define IS_NEGATIVE_NUMBER(ind, arr) (!ind || (arr[ind - 1].type == SIGN ||\
-			(arr[ind - 1].type == BLACKETS && arr[ind - 1].data.symbol == '(')))
+//#define VALID_CHARACTERS "1234567890-+*/^()"
+#define IS_FUNC(s) ((s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z') || s == '_')
 #define IS_SIGN(s) (s == '-' || s == '+' || s == '*' || s == '/' || s == '^')
 #define IS_BLACKETS(s) (s == '(' || s == ')')
-#define IS_FUNC(s) ((s >= 'a' && s <= 'z') || (s >= 'A' && s <= 'Z'))
-static unsigned
-__is_token(unsigned ind, unsigned ind_token, const unsigned char *list_funcs_string)
+#define IS_NUMBER(s) (s >= '0' && s <= '9')
+#define IS_SEPARATOR(s) (s == ' ' || s == '\t')
+static char
+__preproces(void)
 {
-	//const unsigned char allowed_charaters[] = "1234567890-+*/^()sincos\t ";
-	unsigned char symbol = string[ind];
+	unsigned offset = 0, ind = 0;
+	for (; string[ind]; ind++) {
+		string[ind - offset] = string[ind];
+		if (string[ind] == ' ' || string[ind] == '\t') offset++;
+		if (!IS_FUNC(string[ind]) && !IS_SIGN(string[ind]) && !IS_SEPARATOR(string[ind]) &&\
+					!IS_BLACKETS(string[ind]) && !IS_NUMBER(string[ind])) {
+			return 1;
+		}
+	}
+	string[ind - offset] = '\0';
+	return 0;
+}
+
+
+#define IS_NEGATIVE_NUMBER(ind, arr) (!ind || (arr[ind - 1].type == SIGN ||\
+			(arr[ind - 1].type == BLACKETS && arr[ind - 1].data.symbol == '(')))
+static unsigned
+__is_token(unsigned ind, unsigned ind_token)
+{
 	unsigned offset = 0;
+	char symbol = string[ind];
 	if (IS_NUMBER(symbol) || (symbol == '-' && IS_NEGATIVE_NUMBER(ind_token, arr_tokens))) {
 
 		//number or number_float
@@ -121,30 +141,15 @@ __is_token(unsigned ind, unsigned ind_token, const unsigned char *list_funcs_str
 		arr_tokens[ind_token].data.symbol = symbol;
 		offset = 1;
 	} else if (IS_FUNC(symbol)) {
-		unsigned char t_symbol = 0;
-		for (unsigned ind = 0, ind_start, ind_end; list_func_string[ind];) {
-			ind_start = ind;
-			for (ind_end = ind_start + 1; !IS_FUNC(list_func_string[ind_end]); ind_end++);
-			t_sumbol = list_func_string[ind_end];
-			list_func_string[ind_end] = '\0';
-			if (&stringi[ind] == strstr(&string[ind], &list_func_string[ind_start])) {
-				list_func_string[ind_end] = t_symbol;
-				/*
-				
-				 */
-			}
-			list_func_string[ind_end] = t_symbol;
-			if (!list_func_string[ind_end]) ind = ind_end;
-			else {
-			}
-		}
-		goto ERROR_IS_TOKEN;
+
+		//
+		
 	} else {
-		ERROR_IS_TOKEN:
+		//ERROR_IS_TOKEN:
 		arr_tokens[ind_token].type = UNDEFINED_TYPE;
 		arr_tokens[ind_token].data.str = NULL;
 	}
-	return offset;
+	return offset + ind; // возвращаем значение которое индекс должен принять после этой функции
 }
 
 
@@ -160,6 +165,7 @@ int
 main()
 {
 	__input();
+	__preproces();
 	printf("%s\nlen = %ld\n", string, strlen((char *)string));
 	return 0;
 }
